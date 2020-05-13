00c2c6a31dc7f839f6f36a46d03e07b3307d7e27
Aslan <kussein93@gmail.com>
2020-05-14 04:34:30 +0600

    a

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,4 +10,15 @@ export class AppComponent {
 
   constructor() {
   }
+
+  onActivate(event) {
+    let scrollToTop = window.setInterval(() => {
+      let pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 20); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 16);
+  }
 }
