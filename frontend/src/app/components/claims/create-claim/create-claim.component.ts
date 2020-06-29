1bfcbd9ac9bdd834934334b69201db2e734a77e2
Aslan <kussein93@gmail.com>
2020-06-29 13:32:02 +0600

    HTC-464

--- frontend/src/app/components/claims/create-claim/create-claim.component.ts
+++ frontend/src/app/components/claims/create-claim/create-claim.component.ts
@@ -552,17 +552,13 @@ export class CreateClaimComponent implements OnInit, ComponentCanDeactivate, OnD
     }, 800);
   }
 
-  clearClientInfo() {
-    // thapplicationForm.phoneNumber
-  }
-
   searchByPhone(login: string) {
     if (this.util.isNullOrEmpty(login)) return;
     this.subscriptions.add(this.ownerService.searchByPhone(login)
       .subscribe(res => {
         this.fillApplicationFormClientData(res);
         this.existsClient = true;
-      }, () => this.clearApplicationFormClientData()));
+      }));
   }
 
   clearApplicationFormClientData() {
