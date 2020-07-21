ad6aea71ddde1f684442530169c00c865ff0904a
Aslan <kussein93@gmail.com>
2020-07-21 23:27:40 +0600

    HTC-630

--- frontend/src/app/components/claims/claims.component.ts
+++ frontend/src/app/components/claims/claims.component.ts
@@ -94,7 +94,7 @@ export class ClaimsComponent implements OnInit, OnDestroy {
     searchFilter['createDate'] = new Period(this.applicationSearchForm.value.crDateFrom, this.applicationSearchForm.value.crDateTo);
     searchFilter['changeDate'] = new Period(this.applicationSearchForm.value.lastModifyDateFrom, this.applicationSearchForm.value.lastModifyDateTo);
     searchFilter['commentDate'] = new Period(this.applicationSearchForm.value.lastCommentDateFrom, this.applicationSearchForm.value.lastCommentDateTo);
-    searchFilter['operationTypeId'] = this.applicationSearchForm.value.operationTypeId;
+    searchFilter['operationTypeId'] = this.applicationSearchForm.value.operationTypeId?.id;
     searchFilter['applicationStatusList'] = this.applicationSearchForm.value.applicationStatusList;
     searchFilter['text'] = this.applicationSearchForm.value.text;
     searchFilter['applicationId'] = this.applicationSearchForm.value.applicationId;
