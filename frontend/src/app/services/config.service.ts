72b513067fd8515847fd0c39a4d86dadcf9f3a46
syrdevs <syrdevs@gmail.com>
2020-08-17 06:49:18 +0600

    HTC-844

--- frontend/src/app/services/config.service.ts
+++ frontend/src/app/services/config.service.ts
@@ -6,7 +6,6 @@ import {Injectable} from '@angular/core';
 export class ConfigService {
     authUrl = 'https://idp-htc.dilau.kz/auth/realms/htc/protocol/openid-connect/token';
     apiDataManagerUrl = 'https://dm-htc.dilau.kz';
-    // apiDataManagerUrl = 'http://localhost:8081';
     apiFileManagerUrl = 'https://fm-htc.dilau.kz';
     apiViewManagerUrl = 'https://vm-htc.dilau.kz';
     apiUserManagerUrl = 'https://um-htc.dilau.kz';
